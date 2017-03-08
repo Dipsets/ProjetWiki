0a81d682739a6b266139f6a79241d2e777183726
Dipsets <kevin.heylbroeck@student.vinci.be>
2017-03-08 09:06:59 +0100

    Depart

---
+++ ProjetWikipedia/src/Pages.java
@@ -0,0 +1,16 @@
+
+public class Pages {
+	
+	private int id;
+	private int idW;
+	private int taille;
+	private int title;
+	
+	public Pages(int id_i, int idW_i, int taille_i, int title_i){
+		this.id = id_i;
+		this.idW = idW_i;
+		this.taille = taille_i;
+		this.title = title_i;
+	}
+
+}
